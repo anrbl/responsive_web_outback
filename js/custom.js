a329abab9fe50c12c85877ac546c46665b66a136
anrbl <eujin0903@gmail.com>
2023-06-22 13:06:20 +0900

    1

--- js/custom.js
+++ js/custom.js
@@ -8,9 +8,9 @@ $(function () {
     const MainSlide = new Swiper('.MainSlide', {
         loop: true,
         parallax: true,
-        speed: 1000,
+        speed: 1600,
         autoplay: {
-            delay: 4000,
+            delay: 2500,
             disableOnInteraction: false,
         },
         on: {
@@ -21,8 +21,8 @@ $(function () {
                     .siblings()
                     .removeClass('on')
             }
-        },
-    });
+        }
+    })
 
     $('.MainVisual .Arrows .left').on('click', function () {
         MainSlide.slidePrev();
@@ -42,12 +42,19 @@ $(function () {
 
     const ItmSlide = new Swiper('.itm_slide', {
         loop: true,
-        slidesPerView: 2,
-        spaceBetween: 350,
-        speed: 600,
+        slidesPerView: 1,
+        spaceBetween: 0,
+        speed: 900,
         autoplay: {
-            delay: 3500,
+            delay: 4000,
             disableOnInteraction: false,
+        },
+
+        breakpoints: {
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 400,
+            }
         }
     });
     $('.MainItm .arrows .left').on('click', function () {
@@ -62,4 +69,20 @@ $(function () {
     // document.querySelector('.MainItm .arrows .left').addEventListener('click',()=> {
     //     ItmSlide.slidePrev();
     // })
+
+
+
+    $('.mobile_btn').on('click', function () {
+        $(this).toggleClass('on');
+        $('.Gnb').toggleClass('on');
+    });
+    $('.Gnb>ul>li>a').on('click', function (e) {
+        e.preventDefault();
+        $(this).next().stop().slideDown();
+        $(this).parent().siblings().find('.sub').stop().slideUp();
+    });
+
+    $(window).on('resize', function () {
+        $('.Gnb .sub').removeAttr('style')
+    })
 });
